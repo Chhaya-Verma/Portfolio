df73372ab9bc1190c9f01d6175f819ee0efc7c54
Chhaya-Verma <chhayav300@gmail.com>
2025-03-02 13:47:02 +0530

    Add footer section with simple design

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+      <footer className="w-full text-center py-4 bg-black">
+        <p className="text-white text-sm">
+          Made by <span className="text-purple-500">Templyo</span> | Powered by <span className="text-purple-500">Framer</span>
+        </p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
