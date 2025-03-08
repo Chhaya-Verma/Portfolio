c7007c39154b7a22f54ca283ae41053cbc036051
Chhaya-Verma <chhayav300@gmail.com>
2025-03-08 12:18:14 +0530

    first commit with basic sturctures of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from './Navbar';
-import LandingPage from "./LandingPage";
-import Projects from './Projects'; // Importing Projects page component
-import Tools from './Tools'; // Importing Tools page component
-import Experience from './Experience'; // Importing Experience page component
-import Thoughts from './Thoughts'; // Importing Thoughts page component
-import Contact from './Contact'; // Importing Contact page component
-import Faq from "./Faq";
-import Footer from "./Footer";
-const App = () => {
+// import React from "react";
+// import ProfileProjects from "./pages/Projects";
+// import Tools from "./pages/Tools";
+// import Experience from "./pages/Experience";
+// import Thoughts from "./pages/Thoughts";
+// import ContactPage from "./pages/Contact";
+// import LandingPage from "./pages/LandingPage";
+// function App() {
+//   // return <ProfileProjects />;
+//   // return <Tools />;
+//   // return <Experience />;
+// //  return <Thoughts />;
+// // return <ContactPage />;
+// return <LandingPage />;
+ 
+// }
+// export default App;
+
+
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LandingPage from "./pages/LandingPage";
+import ProfileProjects from "./pages/Projects";
+import Tools from "./pages/Tools";
+import Experience from "./pages/Experience";
+import Thoughts from "./pages/Thoughts";
+import ContactPage from "./pages/Contact";
+
+function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects" element={<ProfileProjects />} />
         <Route path="/tools" element={<Tools />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/thoughts" element={<Thoughts />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      <Faq />
       <Footer />
     </Router>
   );
-};
+}
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,87 @@
+// import { Home, Folder, Wrench, Briefcase, Edit, Mail } from "lucide-react";
+// import { Link } from "react-router-dom"; // Importing Link from React Router
+
+// const Navbar = () => {
+//   return (
+//     <div className="fixed top-5 left-1/2 -translate-x-1/2 bg-gray-900 px-4 py-2 flex gap-6 shadow-lg">
+//       {/* Home Icon */}
+//       <Link to="/" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Home size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Home
+//         </div>
+//       </Link>
+
+//       {/* Folder Icon */}
+//       <Link to="/projects" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Folder size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Projects
+//         </div>
+//       </Link>
+
+//       {/* Wrench Icon */}
+//       <Link to="/tools" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Wrench size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Tools
+//         </div>
+//       </Link>
+
+//       {/* Briefcase Icon */}
+//       <Link to="/experience" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Briefcase size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Experience
+//         </div>
+//       </Link>
+
+//       {/* Edit Icon */}
+//       <Link to="/thoughts" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Edit size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Thoughts
+//         </div>
+//       </Link>
+
+//       {/* Mail Icon */}
+//       <Link to="/contact" className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+//         <Mail size={20} />
+//         <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all">
+//           Contact
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+import { Home, Folder, Wrench, Briefcase, Edit, Mail } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-5 left-1/2 -translate-x-1/2 bg-gray-900 px-4 py-2 flex gap-6 shadow-lg rounded-full">
+      <NavItem to="/" icon={<Home size={20} />} label="Home" />
+      <NavItem to="/projects" icon={<Folder size={20} />} label="Projects" />
+      <NavItem to="/tools" icon={<Wrench size={20} />} label="Tools" />
+      <NavItem to="/experience" icon={<Briefcase size={20} />} label="Experience" />
+      <NavItem to="/thoughts" icon={<Edit size={20} />} label="Thoughts" />
+      <NavItem to="/contact" icon={<Mail size={20} />} label="Contact" />
+    </div>
+  );
+};
+
+const NavItem = ({ to, icon, label }) => (
+  <Link to={to} className="group relative p-2 rounded-full text-white hover:bg-purple-600">
+    {icon}
+    <div className="bg-[#28292a] absolute left-1/2 -translate-x-1/2 bottom-[-30px] text-white text-xs opacity-0 group-hover:opacity-100 group-hover:bottom-[-35px] transition-all px-2 py-1 rounded">
+      {label}
+    </div>
+  </Link>
+);
+
+export default Navbar;
